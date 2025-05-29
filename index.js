73a8218cb0e18d34af1d19eb4b8b48c96449a6cd
Sirajum Munira <muniramujaris975@gmail.com>
2025-05-29 18:41:24 +0200

    Added middleware to handle routes and json requests.

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import dishRoutes from './routes/dishRoutes.js';
 
 const app = express();
 dotenv.config();
+
+app.use(express.json());
+app.use('/', dishRoutes);
 
 const PORT = process.env.PORT;
 const CONNECTION_URL = process.env.CONNECTION_URL;
@@ -11,3 +15,7 @@ const CONNECTION_URL = process.env.CONNECTION_URL;
 mongoose.connect(CONNECTION_URL)
      .then(() => console.log('Connected to MongoDB Atlas...'))
      .catch(err =>console.error('Could not connect to MongoDB...'))
+     
+app.listen(PORT, () => {     
+    console.log(`Server running on the port ${PORT}...`);
+ });
