6b1a5bc4272c218537cdb1547d4821b13a79456e
Sirajum Munira <muniramujaris975@gmail.com>
2025-05-29 19:13:41 +0200

    Moved the database connection to a separate file.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import dbConnect from './db.js';
 import dishRoutes from './routes/dishRoutes.js';
 
 const app = express();
@@ -10,12 +10,9 @@ app.use(express.json());
 app.use('/', dishRoutes);
 
 const PORT = process.env.PORT;
-const CONNECTION_URL = process.env.CONNECTION_URL;
 
-mongoose.connect(CONNECTION_URL)
-     .then(() => console.log('Connected to MongoDB Atlas...'))
-     .catch(err =>console.error('Could not connect to MongoDB...'))
-     
+dbConnect();
+
 app.listen(PORT, () => {     
     console.log(`Server running on the port ${PORT}...`);
  });
